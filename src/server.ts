f03d0de17db2a5eb935d30df3d0553ad98f87142
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-06-14 20:25:33 -0300

    configurando plugin fastify

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,11 @@ import fastify from "fastify"
 import { knex } from "./database"
 import crypto from 'node:crypto'
 import {env} from './env'
+import { transactionsRoutes } from "./routes/transactions"
 
 const app = fastify()
 
-app.get('/hello', async()=>{
-// const transactions = await knex('transactions').insert({
-//     id: crypto.randomUUID(),
-//     title: 'Teste transacao',
-//     amount: 1000,
-// }).returning('*')
-
-const transactions = await knex('transactions').select('*').where('amount',300)
-    return transactions
-})
+app.register(transactionsRoutes)
 
 app.listen({
     port: env.PORT,

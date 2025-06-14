75d03bd578c6ad8f8a31158aa36f8f9027a763ac
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-06-14 20:40:33 -0300

    criando transação insert

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,11 +1,28 @@
 import { FastifyInstance } from "fastify"
 import {knex} from "../database"
+import {z} from 'zod'
+import crypto from 'node:crypto'
 
 export async function transactionsRoutes (app: FastifyInstance){
-    app.get('/hello', async()=>{
+    app.post('/', async(request, reply)=>{
+      const createTransactionBodySchema = z.object({
+        title: z.string(),
+        amount: z.number(),
+        type: z.enum(['credit', 'debit']),
+      })
+
+      const {title, amount, type} = createTransactionBodySchema.parse(request.body)
+
+     await knex('transactions').insert({
+        id: crypto.randomUUID(),
+        title,
+        amount: type == 'credit' ? amount: amount * -1,
+      })
 
 
-const transactions = await knex('transactions').select('*').where('amount',1000)
-    return transactions
+      // HTTP Codes: 
+      // 201 - Recurso criado com sucesso
+
+    return reply.status(201).send()
 })
 }

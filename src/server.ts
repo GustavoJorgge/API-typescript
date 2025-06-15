d202679dab779612e3ac30a8b223e301cdfac2f7
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-06-14 22:40:43 -0300

    configurando hook global

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import fastify from "fastify"
 import { knex } from "./database"
 import crypto from 'node:crypto'
-import {env} from './env'
+import { env } from './env'
 import { transactionsRoutes } from "./routes/transactions"
 
 const app = fastify()
+
+app.addHook('preHandler', async (request, reply) => {
+    console.log(`[${request.method}] ${request.url}`)
+})
 
 app.register(transactionsRoutes, {
     prefix: 'transactions',
@@ -12,6 +16,6 @@ app.register(transactionsRoutes, {
 
 app.listen({
     port: env.PORT,
-}).then(() =>{
+}).then(() => {
     console.log('http server running')
 })

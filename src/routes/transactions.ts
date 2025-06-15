921afde41715724b54a65142c4bfa9c010c31477
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-06-14 21:02:05 -0300

    tipando no knex e integrando com typescript

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,28 +1,28 @@
 import { FastifyInstance } from "fastify"
-import {knex} from "../database"
-import {z} from 'zod'
+import { knex } from "../database"
+import { z } from 'zod'
 import crypto from 'node:crypto'
 
-export async function transactionsRoutes (app: FastifyInstance){
-    app.post('/', async(request, reply)=>{
-      const createTransactionBodySchema = z.object({
-        title: z.string(),
-        amount: z.number(),
-        type: z.enum(['credit', 'debit']),
-      })
+export async function transactionsRoutes(app: FastifyInstance) {
+    app.post('/', async (request, reply) => {
+        const createTransactionBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit']),
+        })
 
-      const {title, amount, type} = createTransactionBodySchema.parse(request.body)
+        const { title, amount, type } = createTransactionBodySchema.parse(request.body)
 
-     await knex('transactions').insert({
-        id: crypto.randomUUID(),
-        title,
-        amount: type == 'credit' ? amount: amount * -1,
-      })
+        await knex('transactions').insert({
+            id: crypto.randomUUID(),
+            title,
+            amount: type == 'credit' ? amount : amount * -1,
+        })
 
 
-      // HTTP Codes: 
-      // 201 - Recurso criado com sucesso
+        // HTTP Codes: 
+        // 201 - Recurso criado com sucesso
 
-    return reply.status(201).send()
-})
+        return reply.status(201).send()
+    })
 }

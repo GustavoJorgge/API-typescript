1a2c36f1574d80a2a72ffd66d29a1980a29ebf15
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-06-16 22:07:58 -0300

    configurando banco de teste

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,11 @@
-import 'dotenv/config'
+import {config} from 'dotenv'
 import {z} from 'zod'
+
+if(process.env.NODE_ENV == 'test'){
+    config({path: '.env.test'})
+}else{
+    config()
+}
 
 //colocando um formato padrão que vou receber de dados das variaveis de ambiente
 // process.env
--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -1,25 +1,52 @@
-import { test, beforeAll, afterAll,describe } from 'vitest'
+import { test, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
+import { execSync } from 'node:child_process'
 
-describe('Transaction routes', () =>{
+describe('Transaction routes', () => {
     beforeAll(async () => {
         await app.ready()
     })
 
+    afterAll(async () => {
+        await app.close()
+    })
+
+    beforeEach(() => {
+        execSync('npm run knex migrate:rollback --all')
+        execSync('npm run knex migrate:latest')
+    })
+
     test('O Usuario consegue criar uma nova transação', async () => {
-        await request(app.server)
+        const response = await request(app.server)
             .post('/transactions')
             .send({
                 title: 'New transaction',
                 amount: 5000,
                 type: 'credit',
             }).expect(201)
-    
-    
-    })
-})
 
-afterAll(async () => {
-    await app.close()
+        console.log(response.get('Set-Cookie'))
+    })
+
+    test('Lista todas as transacoes', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 5000,
+                type: 'credit',
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const list = await request(app.server).get('/transactions').set('Cookie', cookies).expect(200)
+
+        expect(list.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New transaction',
+                amount: 5000,
+            })
+        ])
+    })
 })

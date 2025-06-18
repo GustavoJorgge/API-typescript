b8dc76fac30925f135c8c3dfc074e393aece9700
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-06-17 23:19:23 -0300

    debug deploy

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from './app'
 import {env} from './env'
 
-
+const port = Number(process.env.PORT) || env.PORT
 app.listen({
     port: env.PORT,
+    host: '0.0.0.0'
 }).then(() => {
-    console.log('http server running')
+    console.log(`http server running port ${port}` )
 })

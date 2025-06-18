e1f3edabc0fd31182764343e4b052fa2feec571c
Gustavo Jorge <99773984+GustavoJorgge@users.noreply.github.com>
2025-06-17 23:01:28 -0300

    configurando prod

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,15 @@
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
-if(!process.env.DATABASE_URL){
+if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL env NOT FOUND')
 }
 
 export const config: Knex.Config = {
-    client: 'sqlite',
-    connection: {
+    client: env.DATABASE_CLIENT,
+    connection: env.DATABASE_CLIENT == 'sqlite' ? {
         filename: env.DATABASE_URL,
-    },
+    } : env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
         extension: "ts",
